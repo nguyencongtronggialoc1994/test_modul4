1535d94181451777df1d06ee61de6e258c746d11
nguyencongtronggialoc1994 <congtrong94@gmail.com>
2020-12-25 10:49:16 +0700

    add new file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BookListComponent} from './book-list/book-list.component';
+import {CreateBookComponent} from './create-book/create-book.component';
+import {UpdateBookComponent} from './update-book/update-book.component';
+import {BookDetailComponent} from './book-detail/book-detail.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    CreateBookComponent,
+    UpdateBookComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
